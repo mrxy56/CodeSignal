18b73c3530d766676d972814f7c5bfefa4721f16
Tessie <mrxy56@gmail.com>
2023-11-15 15:49:12 -0500

    Add files via upload

---
+++ projectList.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE solution()
+BEGIN
+	/* Write your SQL here. Terminate each statement with a semicolon. */
+	SELECT project_name, team_lead, income
+	FROM Projects
+	ORDER BY internal_id ASC;
+END
