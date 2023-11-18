8b16c9a48feadf61064e1081867e2f5328d9670e
Tessie <mrxy56@gmail.com>
2023-11-18 16:48:51 -0500

    Add files via upload

---
+++ climbingStairs.py
@@ -0,0 +1,12 @@
+# 1. n == 1 and n == 2 are edge cases.
+def solution(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    dp = [0 for i in range(n + 1)]
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    return dp[n]
