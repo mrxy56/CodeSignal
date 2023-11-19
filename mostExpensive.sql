c3a29bae79e66bc3f51b39eab7b2cbc590bb542a
Tessie <mrxy56@gmail.com>
2023-11-19 12:13:36 -0500

    Add files via upload

---
+++ mostExpensive.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE solution()
+BEGIN
+	/* Write your SQL here. Terminate each statement with a semicolon. */
+	SELECT name
+	FROM Products
+	ORDER BY price * quantity DESC, name ASC
+	LIMIT 1 OFFSET 0;
+END
