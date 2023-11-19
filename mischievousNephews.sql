424e951e4d1ff737ef96593bebeb0f1dd1cd0dd5
Tessie <mrxy56@gmail.com>
2023-11-19 18:39:39 -0500

    Add files via upload

---
+++ mischievousNephews.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE solution()
+BEGIN
+	/* Write your SQL here. Terminate each statement with a semicolon. */
+	SELECT WEEKDAY(mischief_date) AS weekday, mischief_date, author, title
+	FROM mischief
+	ORDER BY weekday ASC, FIELD(author, 'Huey', 'Dewey', 'Louie'), mischief_date ASC, title ASC;
+END
